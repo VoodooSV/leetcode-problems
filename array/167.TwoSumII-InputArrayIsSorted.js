eade27926b3e418af96cf6e56cbaf22a3cae3050
VoodooSV <voodoosv@gmail.com>
2019-07-19 09:38:55 +0300

    Add difficulty-value to 167 problem-solution

--- array/167.TwoSumII-InputArrayIsSorted.js
+++ array/167.TwoSumII-InputArrayIsSorted.js
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 167. Two Sum II - Input array is sorted
+Easy
 
 Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
 
